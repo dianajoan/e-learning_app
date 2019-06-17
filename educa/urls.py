f2464c581a1973c60a1d0463de511cd02bde5433
dianajoan <dianajoanita900@gmail.com>
2019-06-17 19:45:57 +0300

    Modifications

--- educa/urls.py
+++ educa/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 	path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
-    path('', CourseListView.as_view(), name='course_list'),
+    #path('', CourseListView.as_view(), name='course_list'),
     path('students/', include('students.urls')),
 ]
 

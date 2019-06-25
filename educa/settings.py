1e1e6ea59cf4431b383dfd85be5fc718c2e7cd0a
dianajoan <dianajoan900@gmail.com>
2019-06-25 15:57:42 +0300

    Added the static folder.

--- educa/settings.py
+++ educa/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from django.urls import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
